eb197d9503f8502556376a2c9f61a98354b94d0c
hywan <devnull@localhost>
2010-05-30 11:22:39 +0000

    Change copyright year.

--- Framework/Library/File/Upload.php
+++ Framework/Library/File/Upload.php
@@ -9,7 +9,7 @@
  * GNU General Public License
  *
  * This file is part of HOA Open Accessibility.
- * Copyright (c) 2007, 2009 Ivan ENDERLIN. All rights reserved.
+ * Copyright (c) 2007, 2010 Ivan ENDERLIN. All rights reserved.
  *
  * HOA Open Accessibility is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published by
@@ -53,7 +53,7 @@ import('File.Exception.Upload_Extension');
  * Manage file uploading from web forms.
  *
  * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright   Copyright (c) 2007, 2009 Ivan ENDERLIN.
+ * @copyright   Copyright (c) 2007, 2010 Ivan ENDERLIN.
  * @license     http://gnu.org/licenses/gpl.txt GNU GPL
  * @since       PHP 5
  * @version     0.3
