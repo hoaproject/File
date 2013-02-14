c3f79cbd013f94f553eae55c20b32d772a576ec1
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-02-14 22:55:02 +0100

    Add the getHash() method.

--- SplFileInfo.php
+++ SplFileInfo.php
@@ -81,7 +81,45 @@ class SplFileInfo extends \SplFileInfo {
      */
     protected $_stream = null;
 
+    /**
+     * Hash.
+     *
+     * @var \Hoa\File\SplFileInfo string
+     */
+    protected $_hash   = null;
 
+
+
+    /**
+     * Construct.
+     *
+     * @access  public
+     * @return  void
+     */
+    public function __construct ( $filename ) {
+
+        parent::__construct($filename);
+
+        $this->_hash = md5(
+            $this->getPathname() .
+            $this->getCTime() .
+            $this->getMTime() .
+            $this->getATime()
+        );
+
+        return;
+    }
+
+    /**
+     * Get the hash.
+     *
+     * @access  public
+     * @return  string
+     */
+    public function getHash ( ) {
+
+        return $this->_hash;
+    }
 
     /**
      * Open the SplFileInfo as a Hoa\File stream.
