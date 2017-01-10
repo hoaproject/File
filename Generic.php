3489bc8f4e3c8944fa5d705d4176fd43646f3ead
Alexis von Glasow <ashgenesis@gmail.com>
2017-01-10 12:56:03 +0100

    Quality: Happy new year!

--- Generic.php
+++ Generic.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2016, Hoa community. All rights reserved.
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -43,7 +43,7 @@ use Hoa\Stream;
  *
  * Describe a super-file.
  *
- * @copyright  Copyright © 2007-2016 Hoa community
+ * @copyright  Copyright © 2007-2017 Hoa community
  * @license    New BSD License
  */
 abstract class Generic
@@ -202,20 +202,20 @@ abstract class Generic
         }
 
         $out .=
-            (($p & 0x0100) ? 'r' : '-')  .
-            (($p & 0x0080) ? 'w' : '-')  .
+            (($p & 0x0100) ? 'r' : '-') .
+            (($p & 0x0080) ? 'w' : '-') .
             (($p & 0x0040) ?
-            (($p & 0x0800) ? 's' : 'x')  :
+            (($p & 0x0800) ? 's' : 'x') :
             (($p & 0x0800) ? 'S' : '-')) .
-            (($p & 0x0020) ? 'r' : '-')  .
-            (($p & 0x0010) ? 'w' : '-')  .
+            (($p & 0x0020) ? 'r' : '-') .
+            (($p & 0x0010) ? 'w' : '-') .
             (($p & 0x0008) ?
-            (($p & 0x0400) ? 's' : 'x')  :
+            (($p & 0x0400) ? 's' : 'x') :
             (($p & 0x0400) ? 'S' : '-')) .
-            (($p & 0x0004) ? 'r' : '-')  .
-            (($p & 0x0002) ? 'w' : '-')  .
+            (($p & 0x0004) ? 'r' : '-') .
+            (($p & 0x0002) ? 'w' : '-') .
             (($p & 0x0001) ?
-            (($p & 0x0200) ? 't' : 'x')  :
+            (($p & 0x0200) ? 't' : 'x') :
             (($p & 0x0200) ? 'T' : '-'));
 
         return $out;
